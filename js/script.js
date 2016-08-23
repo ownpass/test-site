41cc8e446823e99ce7e098684d44c5b1ea31ff69
Walter Tamboer <walter@tamboer.nl>
2016-08-23 21:14:39 +0200

    Added test forms

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(function() {
+    var menu = $('#top-bar-tests-menu');
+
+    menu.append('<li><a href="multiple-forms.html">Multiple Forms</a></li>');
+    menu.append('<li><a href="username-password.html">Username/Password</a></li>');
+    menu.append('<li><a href="identity-credential.html">Identity/Credential</a></li>');
+});
